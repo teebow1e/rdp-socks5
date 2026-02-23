9e303c601be5aa7d5e5f40153d722309da47082a
Trung Tran <tritrungtq@gmail.com>
2026-02-24 00:14:13 +0700

    add support for socks5 with creds

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module app
 
-go 1.17
+go 1.24.0
 
 require h12.io/socks v1.0.3
+
+require golang.org/x/net v0.46.0 // indirect
--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"net"
-	"io"
+	"os"
 	"time"
 
-	"h12.io/socks"
+	"golang.org/x/net/proxy"
 )
 
 var lAddr = flag.String(`l`, `127.0.0.1:3388`, `Listen address`)
-var socksURI = flag.String(`x`, `socks5://127.0.0.1:1080?timeout=15m`, `Socks URI`)
+
+// var socksURI = flag.String(`x`, `socks5://127.0.0.1:1080?timeout=15m`, `Socks URI`)
 var proxyAddr = flag.String(`r`, `10.10.10.10:3389`, `Remote address`)
 var dialFunc func(string, string) (net.Conn, error)
 
@@ -23,7 +26,7 @@ func handleConn(lconn net.Conn) {
 		return
 	}
 	log.Println(`Connected to`, *proxyAddr)
-	defer func () {
+	defer func() {
 		time.Sleep(time.Second)
 		rconn.Close()
 		lconn.Close()
@@ -33,14 +36,46 @@ func handleConn(lconn net.Conn) {
 	log.Println(`Closed:`, lconn.RemoteAddr().String())
 }
 
+func checkSOCKS5Proxy(dialer proxy.Dialer) error {
+	testAddr := "8.8.8.8:53"
+	conn, err := dialer.Dial("tcp", testAddr)
+	if err != nil {
+		return fmt.Errorf("proxy connection failed: %w", err)
+	}
+	defer conn.Close()
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	return nil
+}
+
 func main() {
 	flag.Parse()
-	dialFunc = socks.Dial(*socksURI)
+	socks5ProxyAddr := "xxxx:1080"
+	username := "hehe"
+	password := "cc"
+
+	auth := &proxy.Auth{
+		User:     username,
+		Password: password,
+	}
+
+	proxyDialer, err := proxy.SOCKS5("tcp", socks5ProxyAddr, auth, proxy.Direct)
+	if err != nil {
+		fmt.Printf("Error creating SOCKS5 dialer: %v\n", err)
+		os.Exit(1)
+	}
+
+	err = checkSOCKS5Proxy(proxyDialer)
+	if err != nil {
+		log.Println(err)
+	}
+
+	dialFunc = proxyDialer.Dial
 	srv, err := net.Listen(`tcp`, *lAddr)
 	if err != nil {
 		log.Panicln(err)
 	}
-	log.Println(`Proxing`, *lAddr, `to`, *proxyAddr, `via`, *socksURI)
+	// log.Println(`Proxing`, *lAddr, `to`, *proxyAddr, `via`, *socksURI)
+	log.Println(`Proxing`, *lAddr, `to`, *proxyAddr, `via hardcoded socks`)
 	for {
 		conn, err := srv.Accept()
 		if err != nil {
